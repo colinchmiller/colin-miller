e929d1147070a69ec24723e7f2c9e6c448f5033e
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-06-15 14:21:17 -0500

    add bootstrap and start redesign navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,22 +4,46 @@ import { Link } from "react-router-dom";
 export function Header(props) {
   return (
     <header className={s.header}>
-      <nav className={s.nav}>
-        <ul>
-          <li className={s.nav_item}>
-            <Link to="/">Home</Link>
+      <nav className={`navbar navbar-expand-lg`}>
+        <ul className={`navbar-nav me-auto mb-2 mb-lg-0`}>
+          <li className={`nav-item`}>
+            <Link className={`nav-link`} to="/">
+              Home
+            </Link>
           </li>
-          <li className={s.nav_item}>
-            <Link to="/about">About Me</Link>
+          <li className={`nav-item`}>
+            <Link className={`nav-link`} to="/about">
+              About
+            </Link>
           </li>
-          <li className={s.nav_item}>
-            <Link to="/experience">My Experience</Link>
+          <li className={`nav-item`}>
+            <Link className={`nav-link`} to="/experience">
+              Experience
+            </Link>
           </li>
-          <li className={s.nav_item}>
-            <Link to="/skills">My Skills</Link>
+          <li className={`nav-item`}>
+            <Link className={`nav-link`} to="/skills">
+              Skills
+            </Link>
           </li>
         </ul>
       </nav>
+      {/* <nav className={`navbar navbar-expand-lg ${s.nav}`}>
+        <ul className={`navbar-nav`}>
+          <li className={`nav-item ${s.nav_item}`}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={`nav-item ${s.nav_item}`}>
+            <Link to="/about">About Me</Link>
+          </li>
+          <li className={`nav-item ${s.nav_item}`}>
+            <Link to="/experience">My Experience</Link>
+          </li>
+          <li className={`nav-item ${s.nav_item}`}>
+            <Link to="/skills">My Skills</Link>
+          </li>
+        </ul>
+      </nav> */}
     </header>
   );
 }
