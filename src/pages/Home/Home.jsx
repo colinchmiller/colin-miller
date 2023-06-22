5f6e3bf0bf9f083ae146c95afa8e1be51312360b
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-06-22 09:40:14 -0500

    prep word cloud grid with all taking up fill row width

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,19 +23,45 @@ export function Home(props) {
 
               <div className={s.word_cloud}>
                 <div className={s.cloud_content}>
-                  <h4 data-weight="5">Bootstrap</h4>
-                  <h4 data-weight="8">JavaScript</h4>
-                  <h4 data-weight="5">React</h4>
-                  <h4 data-weight="9">CSS</h4>
-                  <h4 data-weight="3">Node.js</h4>
-                  <h4 data-weight="5">AEM</h4>
-                  <h4 data-weight="7">jQuery</h4>
-                  <h4 data-weight="3">WordPress</h4>
-                  <h4 data-weight="1">Sitecore</h4>
-                  <h4 data-weight="5">Git</h4>
-                  <h4 data-weight="3">Firebase</h4>
-                  <h4 data-weight="9">HTML</h4>
-                  <h4 data-weight="1">Shopify</h4>
+                  <h4 id={s.bootstrap} data-weight="5">
+                    Bootstrap
+                  </h4>
+                  <h4 id={s.javascript} data-weight="8">
+                    JavaScript
+                  </h4>
+                  <h4 id={s.react} data-weight="5">
+                    React
+                  </h4>
+                  <h4 id={s.css} data-weight="9">
+                    CSS
+                  </h4>
+                  <h4 id={s.node} data-weight="3">
+                    Node
+                  </h4>
+                  <h4 id={s.aem} data-weight="5">
+                    AEM
+                  </h4>
+                  <h4 id={s.jquery} data-weight="7">
+                    jQuery
+                  </h4>
+                  <h4 id={s.wordpress} data-weight="3">
+                    WordPress
+                  </h4>
+                  <h4 id={s.sitecore} data-weight="1">
+                    Sitecore
+                  </h4>
+                  <h4 id={s.git} data-weight="5">
+                    Git
+                  </h4>
+                  <h4 id={s.firebase} data-weight="3">
+                    Firebase
+                  </h4>
+                  <h4 id={s.html} data-weight="9">
+                    HTML
+                  </h4>
+                  <h4 id={s.shopify} data-weight="1">
+                    Shopify
+                  </h4>
                 </div>
               </div>
             </div>
