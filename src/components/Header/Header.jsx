0672df4b648357aee0e392ec3a141a46f15c1bf2
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-07-12 21:48:56 -0500

    begin new header design -- circle with expand on click; basic shape and usestate click

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,36 @@
 import s from "./style.module.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export function Header(props) {
+export function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  function toggleHeader() {
+    console.log("click header");
+    setIsActive(true);
+  }
   return (
-    <header className={s.header}>
-      <nav className={`navbar navbar-expand-sm bg-primary-subtle`}>
-        <div className="container-fluid">
-          <Link className={`navbar-brand`} to="/intro">
-            Colin Miller
-          </Link>
-          <ul className={`navbar-nav me-auto`}>
-            <li className={`nav-item`}>
-              <Link className={`nav-link`} to="/experience">
-                Experience
-              </Link>
-            </li>
-            <li className={`nav-item`}>
-              <Link className={`nav-link`} to="/about">
-                About me
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+    <header onClick={toggleHeader} className={s.header}></header>
+    // <header className={s.header}>
+    //   <nav className={`navbar navbar-expand-sm bg-primary-subtle`}>
+    //     <div className="container-fluid">
+    //       <Link className={`navbar-brand`} to="/intro">
+    //         Colin Miller
+    //       </Link>
+    //       <ul className={`navbar-nav me-auto`}>
+    //         <li className={`nav-item`}>
+    //           <Link className={`nav-link`} to="/experience">
+    //             Experience
+    //           </Link>
+    //         </li>
+    //         <li className={`nav-item`}>
+    //           <Link className={`nav-link`} to="/about">
+    //             About me
+    //           </Link>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </nav>
+    // </header>
   );
 }
