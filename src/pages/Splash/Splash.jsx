11efcb46403df051b7565f43df6d34134650b090
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-07-17 14:49:01 -0500

    style and animate nav menu items

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -6,9 +6,6 @@ export function Splash() {
       <h1>Colin Miller</h1>
       <h2>Web Developer</h2>
       <h3>Minneapolis, Minnesota</h3>
-      <button className="btn btn-primary" href="/about">
-        Learn more about me
-      </button>
     </div>
   );
 }
