8e47fcda4041e49ce6149ec67a31e311045a6cd9
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-07-17 11:03:32 -0500

    animate menu open/close and splash text fadein/slideup

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -6,7 +6,7 @@ export function Splash() {
       <h1>Colin Miller</h1>
       <h2>Web Developer</h2>
       <h3>Minneapolis, Minnesota</h3>
-      <button className="btn btn-primary" href="#">
+      <button className="btn btn-primary" href="/about">
         Learn more about me
       </button>
     </div>
