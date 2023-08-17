c71b3947a8240489d316b6555b1df5e82126ff6f
Colin Miller <7650338+colinchmiller@users.noreply.github.com>
2023-08-17 16:22:57 -0500

    replace h1 splash with static svg

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,9 +1,10 @@
+import { ReactComponent as ColinLogo } from "../../assets/images/colin-miller.svg";
 import s from "./style.module.css";
 
 export function Splash() {
   return (
     <div className={s.splash}>
-      <h1>Colin Miller</h1>
+      <ColinLogo />
       <h2>Web Developer</h2>
       <h3>Minneapolis, Minnesota</h3>
       <div className={s.button}>
